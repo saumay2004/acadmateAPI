221863e5592f510245e3aeac0fad25af256e5ed5
Saumay.G <144949928+Saumay-Goel@users.noreply.github.com>
2024-10-20 02:55:22 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      secure: true,
+      secure: false,
       sameSite: "Strict",
       maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
