f86088b6ca7301d4fd5f706d54f899d16264b328
Saumay.G <144949928+Saumay-Goel@users.noreply.github.com>
2024-10-22 21:51:08 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use(
 
 app.use(helmet());
 
-const allowedOrigins = ["https://acadbud.vercel.app", "http://localhost:3000"];
+const allowedOrigins = ["https://acadbud.vercel.app", "http://localhost:3000", "https://saumay.shashstorm.in/login"];
 app.use(
   cors({
     origin: function (origin, callback) {
