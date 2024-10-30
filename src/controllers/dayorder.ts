b8b1ee0f10424e50c9e6472baa7742e1b1428fd7
SaumayGoel <sg0627@srmist.edu.in>
2024-10-31 00:56:07 +0530

    fix: DO fix

--- src/controllers/dayorder.ts
+++ src/controllers/dayorder.ts
@@ -35,7 +35,7 @@ export async function Order(req: Request, res: Response) {
       const decodedHTML = decodeEncodedString(OrderResponse.data);
       const dayOrder = extractDayOrder(decodedHTML);
 
-      if (dayOrder !== null) {
+      if (dayOrder) {
         res.status(200).json({ dayOrder });
       } else {
         console.log("Day Order not found");
